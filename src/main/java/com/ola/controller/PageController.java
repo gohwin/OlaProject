6cfa79186f22a47b535f0fad1ab919fe4b73d652
gowin <gohs9167@naver.com>
2023-11-27 15:02:44 +0900

    main

--- src/main/java/com/ola/controller/PageController.java
+++ src/main/java/com/ola/controller/PageController.java
@@ -7,14 +7,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping
 public class PageController {
-	
+
 	@GetMapping("/main")
 	public void mainView() {
 
 	}
-	
-	@GetMapping("/clothes")
-	public void ClothesView() {
 
+	@GetMapping("/item/clothes")
+	public String showClothesPage() {
+		return "item/clothes";
+	}
+
+	@GetMapping("/item/pants")
+	public String showPantsPage() {
+		return "item/pants";
+	}
+
+	@GetMapping("/item/shoes")
+	public String showShoesPage() {
+		return "item/shoes";
+	}
+
+	@GetMapping("/item/accessories")
+	public String showAccessoriesPage() {
+		return "item/accessories";
+	}
+
+	@GetMapping("/item/sales")
+	public String showsalesPage() {
+		return "item/sales";
 	}
 }
