508529644e5254ee27545e39b04df9b86110da68
gowin <gohs9167@naver.com>
2023-12-04 14:44:21 +0900

    getboard 포기

---
+++ src/main/java/com/ola/service/OrderListService.java
@@ -0,0 +1,26 @@
+package com.ola.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ola.entity.Member;
+import com.ola.entity.OrderList;
+import com.ola.repository.OrderListRepository;
+
+@Service
+public class OrderListService {
+
+    private final OrderListRepository orderListRepository;
+
+    @Autowired
+    public OrderListService(OrderListRepository orderListRepository) {
+        this.orderListRepository = orderListRepository;
+    }
+
+    // 특정 회원의 주문 내역 조회
+    public List<OrderList> getOrderHistory(Member member) {
+        return orderListRepository.findByMember(member);
+    }
+}
