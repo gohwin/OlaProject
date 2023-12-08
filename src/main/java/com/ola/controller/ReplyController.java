dd8bc10987ed5358954db664187ea47a5621c14c
jangs <jangs@192.168.219.106>
2023-12-08 20:50:00 +0900

    댓글 DTO 삭제 및 레포지토리 처리

--- src/main/java/com/ola/controller/ReplyController.java
+++ src/main/java/com/ola/controller/ReplyController.java
@@ -5,32 +5,43 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.ola.DTO.ReplyDTO;
+import com.ola.entity.Community;
 import com.ola.entity.Member;
+import com.ola.entity.Reply;
+import com.ola.repository.CommunityRepository;
+import com.ola.repository.ReplyRepository;
 import com.ola.security.SecurityUser;
-import com.ola.service.ReplyService;
 
 @Controller
 public class ReplyController {
 	@Autowired
-	private ReplyService replyService;
+	private ReplyRepository replyRepo;
+	
+	@Autowired
+	private CommunityRepository commuRepo;
 
 	@PostMapping("/addReply")
-	public String addReply(@ModelAttribute ReplyDTO replyDTO, Authentication authentication) {
-		SecurityUser userDetails = (SecurityUser) authentication.getPrincipal();
-		Member currentMember = userDetails.getMember();
+    public String addReply(@RequestParam("communityNo") Long communityNo,
+                           @RequestParam("replycontent") String content,
+                           Authentication authentication) {
+        SecurityUser userDetails = (SecurityUser) authentication.getPrincipal();
+        Member currentMember = userDetails.getMember();
 
-		// 댓글 등록을 위한 필요한 정보 설정
-		replyDTO.setMemberName(currentMember.getName());
-		replyDTO.setRegDate(new Date());
+        Community community = commuRepo.findById(communityNo)
+                              .orElseThrow(() -> new IllegalArgumentException("Invalid community No:" + communityNo));
 
-		replyService.addReply(replyDTO);
+        Reply reply = new Reply();
+        reply.setMember(currentMember);
+        reply.setCommunity(community);
+        reply.setContent(content);
+        reply.setRegDate(new Date()); // Assuming you have a regDate field for the registration date
 
-		// 댓글 등록 후, 해당 게시글로 리다이렉트
-		return "redirect:/getCommuBoard?communityNo=" + replyDTO.getCommunityNo();
-	}
+        replyRepo.save(reply);
+
+        return "redirect:/getCommuBoard?communityNo=" + communityNo;
+    }
 
 }
