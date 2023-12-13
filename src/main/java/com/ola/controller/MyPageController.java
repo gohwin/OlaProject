03470b8d2967943ccc1d2626d6bbd8f5c21e15e4
SeonHo <jangsh4752@naver.com>
2023-12-13 13:25:02 +0900

    장바구니 삭제 기능 추가

--- src/main/java/com/ola/controller/MyPageController.java
+++ src/main/java/com/ola/controller/MyPageController.java
@@ -2,8 +2,8 @@ package com.ola.controller;
 
 import java.util.List;
 
-import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ola.entity.Basket;
 import com.ola.entity.Member;
-import com.ola.entity.OrderList;
 import com.ola.repository.BasketRepository;
 import com.ola.repository.OrderListRepository;
 import com.ola.security.SecurityUser;
-import com.ola.service.OrderListService;
 import com.ola.service.UserService;
 
 @Controller
@@ -86,6 +85,28 @@ public class MyPageController {
 	    // 장바구니 페이지의 Thymeleaf 템플릿 파일명을 반환합니다.
 	    return "mypage/basket";
 	}
+	
+	@PostMapping("/deleteBasket")
+    public ResponseEntity<?> removeProductFromBasket(@RequestParam Long productNo, @AuthenticationPrincipal SecurityUser principal) {
+        // 현재 로그인한 사용자 정보를 가져옵니다.
+        Member member = principal.getMember();
+
+        // 사용자의 장바구니를 조회합니다.
+        Basket basket = basketRepo.findByUser(member);
+
+        // 장바구니에서 해당 상품을 찾아 삭제합니다.
+        // 이 부분은 당신의 도메인 모델에 따라 다를 수 있습니다.
+        basket.removeProduct(productNo);
+
+        // 변경된 장바구니를 저장합니다.
+        basketRepo.save(basket);
+
+        // 클라이언트에 성공 메시지를 보냅니다.
+        return ResponseEntity.ok("상품이 성공적으로 삭제되었습니다.");
+    }
+
+
+
 
 
 
